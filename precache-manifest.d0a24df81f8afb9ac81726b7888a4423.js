56ac8b29ca67abe8cc63c33893d7d7f8b1e5cbe9
Nimesh Shrestha <nimesh.shrestha@hotmail.com>
2020-02-20 17:27:05 -0800

    Updates

---
+++ precache-manifest.d0a24df81f8afb9ac81726b7888a4423.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45162a2854cd586e2a24c45f4dd69ccc",
+    "url": "/jeopardy-frontend/index.html"
+  },
+  {
+    "revision": "a41d5877fa48f190185f",
+    "url": "/jeopardy-frontend/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6812ecf21e316717931a",
+    "url": "/jeopardy-frontend/static/js/2.93698cf4.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/jeopardy-frontend/static/js/2.93698cf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a41d5877fa48f190185f",
+    "url": "/jeopardy-frontend/static/js/main.763f1856.chunk.js"
+  },
+  {
+    "revision": "a0f9bf9f4335c67c246f",
+    "url": "/jeopardy-frontend/static/js/runtime-main.b2385aae.js"
+  }
+]);
